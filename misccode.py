24b6fd0d7b691ae1ab81c338c9d0924938fed66d
Kishalay <kishalaydas@iisc.ac.in>
2020-07-22 23:41:06 +0530

    22nd July Commit

--- misccode.py
+++ misccode.py
@@ -31,3 +31,42 @@ file = open("atom_index_features.csv", 'w', newline='')
 with file:
   wr = csv.writer(file)
   wr.writerows(index_feature)
+
+
+
+# Tsne Plot
+
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+def tsneplot(embeddings,labels,fig_path):
+    print("********************* tSNE Plot*********************")
+    X = TSNE(n_components=2,perplexity=100,n_iter=1000).fit_transform(embeddings)
+    colors = ['#FF0000', '#06D506', '#0931F7', '#00FFFF', '#FFE500', '#F700FF', '#9300FF', '#FFD700','#10DADE']   # Red , Green, Blue
+    for c in range(len(colors)):
+        points = []
+        for j in range(len(labels)):
+            if (labels[j] == c):
+                points.append(list(X[j]))
+        x = []
+        y = []
+        for z in points:
+            x.append(z[0])
+            y.append(z[1])
+        plt.plot(x, y, 'ro', c=colors[c], markersize=20, marker='.')
+    plt.axis('off')
+    plt.savefig(fig_path)
+    plt.close()
+
+# Spectral CLustering
+
+from sklearn.cluster import SpectralClustering
+def spectral_clustering(adj,num_cluster):
+    clustering = SpectralClustering(n_clusters=num_cluster, affinity='precomputed', assign_labels="discretize",
+                                    random_state=42).fit(adj)
+    return clustering.labels_
+
+# read .mtx file
+import scipy as scp
+source_adj_path = "../data/polblogs/polblogs.mtx"
+adj=scp.io.mmread(source_adj_path).todense()
+
